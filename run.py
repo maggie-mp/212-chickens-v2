8e5c5462744f8eaecf6caabfb843d618843b4769
maggie-mp <magsmcperry@gmail.com>
2022-08-05 21:28:36 +1200

    redoing py

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
-
-
-burgers = [
- 'Classic Burger $4.99',
- 'Cheese Burger $5.99',
- 'Chicken Burger $5.99',
- 'Double Burger $6.99'
-]
-
 
 @app.route('/')
 def index():
-    return render_template('index.html', disclaimer='may contain traces of nuts')
+    return('<h1>hello world</h1>')
