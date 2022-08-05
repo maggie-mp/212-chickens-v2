04b02f9589d57e3e1a6e7fa892715e9a6dc8ce0b
maggie-mp <magsmcperry@gmail.com>
2022-08-06 08:20:12 +1200

    got control structures working

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+burgers = [
+ ['Classic Burger', '$4.99'],
+ ['Cheese Burger', '$5.99'],
+ ['Chicken Burger', '$5.99'],
+ ['Double Burger', '$6.99']
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html', disclaimer='may contain traces of nuts')
+    return render_template('index.html',
+                            disclaimer='may contain traces of nuts',
+                            burgers=burgers)
