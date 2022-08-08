a5f0da0348290930a00c776d473ce5b17cb0127e
maggie-mp <magsmcperry@gmail.com>
2022-08-08 18:58:26 +1200

    questions on python set up

--- run.py
+++ run.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-questions = [
- ['Question 1:'],
+questionss = [
+ ['Question 1:', 'How often do you want your chickens to lay?'],
+ #['Question 2:', 'How big do you want your chickens to be (in mass)?'],
+ #[['Question 3:', 'How friendly do you want your chickens to be?'],
+ #['Question 4:', 'How big is your current backyard?'],
+ #['Question 5:', 'Do you get wild weather/strange climate where you live?'],
 ]
 
 answers = [
-[['Rarely', 'Summer & Spring', 'All Year Round']]
+[['Rarely', 'Summer & Spring', 'All Year Round']],
+#[['Light Breed', 'Medium Breed', 'Heavy Breed']],
+#[['Friendly', 'Child Friendly']],
+#[['Small', 'Medium', 'Large']],
+#[['It's very dry, 'It gets rain/snow often', 'Neither']],
 ]
 
 @app.route('/')
@@ -20,5 +28,5 @@ def index():
 def questions():    
     return render_template('questions.html',
                             disclaimer='This website is for folks living in Aotearoa, New Zealand | Designed and Coded by Maggie McMillan-Perry',
-                            questions=questions,
+                            questionss=questionss,
                             answers=answers)    
