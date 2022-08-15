7aa779198aa90598c1a44839a68d1f0879ab468b
maggie-mp <magsmcperry@gmail.com>
2022-08-15 16:14:25 +1200

    worked out q's and a's changing

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, session, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = 'CHICKENS'
 
 MENUDB = 'chickens.db'
 
@@ -9,34 +10,42 @@ MENUDB = 'chickens.db'
 #['Red Shavers','Heavy Breed','All Year Round', 'Friendly', 'Not Fussy', 'Small'],
 #]
 
-questionss = [
- ['Question 1:', 'How often do you want your chickens to lay?'],
- #['Question 2:', 'How big do you want your chickens to be (in mass)?'],
- #[['Question 3:', 'How friendly do you want your chickens to be?'],
- #['Question 4:', 'How big is your current backyard?'],
- #['Question 5:', 'Do you get wild weather/strange climate where you live?'],
-]
-
-answers = [
-[['Rarely', 'Summer & Spring', 'All Year Round']],
-#[['Light Breed', 'Medium Breed', 'Heavy Breed']],
-#[['Friendly', 'Child Friendly']],
-#[['Small', 'Medium', 'Large']],
-#[['It's very dry/hot, 'It gets rain/cold often', 'Neither']],
-]
-
 @app.route('/index')
+@app.route('/')
 def index():
-    return render_template('index.html',
-                            disclaimer='This website is for folks living in Aotearoa, New Zealand | Designed and Coded by Maggie McMillan-Perry',
-                            answers=answers)
+    session['question'] = 0
+    session['questions_list'] = []
+    session['answers_list'] = []
+    db = sqlite3.connect(MENUDB)
+    cur = db.execute('SELECT * FROM questions')
+    for row in cur:
+        session['questions_list'].append(list(row))
+    cur = db.execute('SELECT * FROM answers')
+    for row in cur:
+        session['answers_list'].append(list(row))
+    db.close()
+    
+    return render_template('index.html')
+
+    # return redirect(url_for('questions'))
 
 @app.route('/questions')
-def questions():    
+def questions():
+    if not 'question' in session:
+        return redirect(url_for('index'))
     return render_template('questions.html',
                             disclaimer='This website is for folks living in Aotearoa, New Zealand | Designed and Coded by Maggie McMillan-Perry',
-                            questionss=questionss,
-                            answers=answers)    
+                            questionss=session['questions_list'],
+                            answers=session['answers_list'],
+                            current_question=session['question'])    
+
+@app.route('/egg', methods=['GET', 'POST'])
+def egg():
+    session['question'] += 1
+    if session['question'] >= 5:
+        return redirect(url_for('results'))
+    else:
+        return redirect(url_for('questions'))
 
 @app.route('/results')
 def results():  
